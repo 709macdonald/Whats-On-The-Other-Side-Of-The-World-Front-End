b084d959e8e1d2457b07e53df885f4edbef284a4
709macdonald <709macdonald@gmail.com>
2025-03-08 20:56:14 -0330

    Search bar basics

---
+++ whats_on_the_other_side_of_the_world/src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SearchBar() {
+  return (
+    <div className="searchDiv">
+      <h3>Enter Your Current Location</h3>
+      <input placeholder="Whats on the other side of..."></input>
+      <button>Search</button>
+    </div>
+  );
+}
