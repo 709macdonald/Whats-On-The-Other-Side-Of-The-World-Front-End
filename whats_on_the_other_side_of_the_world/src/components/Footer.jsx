c39050a3ac8b9a9e7a940506761062ad3f4fd9e8
709macdonald <709macdonald@gmail.com>
2025-03-08 20:55:15 -0330

    Footer done

---
+++ whats_on_the_other_side_of_the_world/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <button className="resetButton">Reset</button>
+      <button className="McDonaldsButton">Nearest McDonalds</button>
+    </div>
+  );
+}
