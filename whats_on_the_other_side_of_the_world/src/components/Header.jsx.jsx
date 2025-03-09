f4b7ba36e716fef1ac808798d0cc26ab02ccf15a
709macdonald <709macdonald@gmail.com>
2025-03-08 20:55:39 -0330

    Header Done

--- whats_on_the_other_side_of_the_world/src/components/Header.jsx.jsx
+++ whats_on_the_other_side_of_the_world/src/components/Header.jsx.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Where In The World?</h1>
+      <p>Find out whats directly below your feet right now.</p>
+    </div>
+  );
+}
